f735305133fcb1ef5e206c320b10223c2b27b1af
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2019-08-16 22:06:32 +0200

    micro woodworking cam

--- teile.py
+++ teile.py
@@ -1,61 +1,123 @@
-from collections import namedtuple
+import attr
+import enum
 from collections import Counter
 
-Brett = namedtuple("Brett", "dick breit lang")
+
+class MaterialTyp(enum.Enum):
+    BRETT = enum.auto()
+    SCHREINERPLATTE = enum.auto()
+    SPERRHOLZ = enum.auto()
 
 
-def aussen(lang, breit):
-    return Brett(lang=max(lang, breit), breit=min(lang, breit), dick=DICKE)
+class MagicalListMixin:
+    def __mul__(self, number):
+        return [self] * number
+
+@attr.s(frozen=True)
+class Material:
+    name = attr.ib(cmp=False)
+    dick = attr.ib()
+    breit = attr.ib()
+    typ = attr.ib()
+
+    def brett(self, lang):
+        return Brett(self, lang)
 
 
-def innen(lang):
-    return Brett(lang=lang, breit=BREITE_INNEN, dick=DICKE_INNEN)
+@attr.s(frozen=True)
+class Brett(MagicalListMixin):
+    material = attr.ib()
+    lang = attr.ib()
+
+    def __str__(self):
+        return f"{self.material.name}-{self.breit}x{self.lang}"
+
+    @property
+    def breit(self):
+        return self.material.breit
+
+    @property
+    def dick(self):
+        return self.material.dick
+
+
+@attr.s(frozen=True)
+class StückGut(MagicalListMixin):
+    name = attr.ib()
+
+    def __str__(self):
+        return self.name
 
 
 SPIEL = 5
+ELEMENT = 1000
+WANDÜBERSTAND = 30
+FRONTÜBERSTAND = 10
+HÖHE = 400
 
-DICKE = 24
-
-DICKE_INNEN = 22
-BREITE_INNEN = 150
-
-ELEMENTBREITE = 1000
-BASIS_HOCH = 400
-SEITENBREITE = 800
-
-DECKBREITE = ELEMENTBREITE // 2
-
-
-HINTERWAND = aussen(breit=ELEMENTBREITE, lang=BASIS_HOCH)
-DECKTEIL = aussen(breit=DECKBREITE, lang=ELEMENTBREITE)
-SEITENTEIL = aussen(breit=SEITENBREITE, lang=BASIS_HOCH)
-
-FRONTWAND = aussen(breit=HINTERWAND.breit - SPIEL // 2, lang=HINTERWAND.lang - SPIEL)
-
-AUSSPAARUNGSBREITE = (ELEMENTBREITE - 3 * DICKE) // 2 - SPIEL
-
-HINTEN_SCHUBLADE = innen(AUSSPAARUNGSBREITE)
-SEITE_SCHUBLADE = innen(SEITENBREITE - DICKE_INNEN - SPIEL)
+KOEPUS_DÜBEL = StückGut("HolzdübelKorpus")
+FACH_DÜBEL = StückGut("HolzdübelFach")
+VERBINDUNGS_BOLZEN = StückGut("Scraubverbindung")
+FACH_SCHRAUBE = StückGut("Fachschraube") # sollen auch front an fach bringen
+KORPUS_SCHRAUBE = StückGut("Korpusschraube") # bringen seitenteile an front
+ELEMENT_VERBINDER = StückGut("ElementVerbinder")
 
 
-SCHUBLADE = [HINTEN_SCHUBLADE, FRONTWAND] + [SEITE_SCHUBLADE] * 2
+def teileliste(*, korpus, fach_kanten, deck, front, seitentbreite):
+
+    hinterwand = korpus.brett(ELEMENT)
+    deckteil = deck.brett(ELEMENT)
+
+    seitenteil = korpus.brett(seitentbreite)
+
+    front_teil = front.brett(hinterwand.lang - SPIEL // 2)
+
+    breite_ausspaarung = (ELEMENT - 3 * korpus.dick) // 2 - SPIEL
+
+    schublade_breite = fach_kanten.brett(breite_ausspaarung)
+    schublade_seite = fach_kanten.brett(seitenteil.lang - 2 * fach_kanten.dick - SPIEL)
+
+    # todo: schublade brett
+    schublade = [front_teil] + FACH_SCHRAUBE * 3 + [schublade_breite] * 2 + [schublade_seite] * 2
+
+    element = [hinterwand] + [deckteil] * 2 + [seitenteil] * 3 + schublade * 2
+
+    # SEITENFUELLER = aussen(lang=BASIS_HOCH, breit=DICKE)
+
+    # DECKENFUELLER = aussen(lang=DECKBREITE, breit=DICKE)
+
+    return element * 2  # + [SEITENFUELLER, DECKENFUELLER] * 2
 
 
-ELEMENT = [HINTERWAND] + [DECKTEIL] * 2 + [SEITENTEIL] * 3 + SCHUBLADE * 2
+SCHICHTHOLZ_27_KORPUS = Material(
+    name="Schichtholz_27",
+    dick=27,
+    breit=HÖHE,
+    typ=MaterialTyp.SCHREINERPLATTE,
+)
+SCHICHTHOLZ_27_DECK = attr.evolve(SCHICHTHOLZ_27_KORPUS, breit=ELEMENT // 2)
+
+SCHICHTHOLZ_27_FRONT = attr.evolve(SCHICHTHOLZ_27_KORPUS, breit=HÖHE - SPIEL)
 
 
-SEITENFUELLER = aussen(lang=BASIS_HOCH, breit=DICKE)
+FICHTE_200_18 = Material(
+    name="Fichtenbrett_200x18",
+    dick=18,
+    breit=200,
+    typ=MaterialTyp.BRETT,
+)
 
-DECKENFUELLER = aussen(lang=DECKBREITE, breit=DICKE)
 
-
-ALLES = ELEMENT * 2 + [SEITENFUELLER, DECKENFUELLER] * 2
-
-
-counted = Counter(ALLES)
+counted = Counter(teileliste(
+    korpus=SCHICHTHOLZ_27_KORPUS,
+    deck=SCHICHTHOLZ_27_DECK,
+    front=SCHICHTHOLZ_27_FRONT,
+    fach_kanten=FICHTE_200_18,
+    seitentbreite=800,  # TODO: ausrechnen
+))
 teile = counted.most_common()
 
-#teile.sort(key=lambda x: x[0.])
+# teile.sort(key=lambda x: x[0.])
 
 for item, count in counted.most_common():
     print(item, "x", count)
