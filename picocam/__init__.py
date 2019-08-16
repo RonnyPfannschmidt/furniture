2bb1987a1233e3ffef9fbf2336069c5aafe7ae72
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2019-08-16 23:39:04 +0200

    move types to picocam

---
+++ picocam/__init__.py
@@ -0,0 +1,53 @@
+import attr
+import enum
+
+class MaterialTyp(enum.Enum):
+    BRETT = enum.auto()
+    SCHREINERPLATTE = enum.auto()
+    SPERRHOLZ = enum.auto()
+
+
+class MagicalListMixin:
+    def __mul__(self, number):
+        return [self] * number
+
+    def __add__(self, other):
+        return [self] + other
+
+    def __radd___(self, other):
+        return other * [self]
+
+@attr.s(frozen=True)
+class Material:
+    name = attr.ib(cmp=False)
+    dick = attr.ib()
+    breit = attr.ib()
+    typ = attr.ib()
+
+    def brett(self, lang):
+        return Brett(self, lang)
+
+
+@attr.s(frozen=True)
+class Brett(MagicalListMixin):
+    material = attr.ib()
+    lang = attr.ib()
+
+    def __str__(self):
+        return f"{self.material.name}-{self.breit}x{self.lang}"
+
+    @property
+    def breit(self):
+        return self.material.breit
+
+    @property
+    def dick(self):
+        return self.material.dick
+
+
+@attr.s(frozen=True)
+class StückGut(MagicalListMixin):
+    name = attr.ib()
+
+    def __str__(self):
+        return self.name
--- teile.py
+++ teile.py
@@ -1,58 +1,6 @@
 import attr
-import enum
 from collections import Counter
-
-
-class MaterialTyp(enum.Enum):
-    BRETT = enum.auto()
-    SCHREINERPLATTE = enum.auto()
-    SPERRHOLZ = enum.auto()
-
-
-class MagicalListMixin:
-    def __mul__(self, number):
-        return [self] * number
-
-    def __add__(self, other):
-        return [self] + other
-
-    def __radd___(self, other):
-        return other * [self]
-
-@attr.s(frozen=True)
-class Material:
-    name = attr.ib(cmp=False)
-    dick = attr.ib()
-    breit = attr.ib()
-    typ = attr.ib()
-
-    def brett(self, lang):
-        return Brett(self, lang)
-
-
-@attr.s(frozen=True)
-class Brett(MagicalListMixin):
-    material = attr.ib()
-    lang = attr.ib()
-
-    def __str__(self):
-        return f"{self.material.name}-{self.breit}x{self.lang}"
-
-    @property
-    def breit(self):
-        return self.material.breit
-
-    @property
-    def dick(self):
-        return self.material.dick
-
-
-@attr.s(frozen=True)
-class StückGut(MagicalListMixin):
-    name = attr.ib()
-
-    def __str__(self):
-        return self.name
+from picocam import MaterialTyp, Material, StückGut
 
 
 SPIEL = 5
@@ -70,7 +18,6 @@ ELEMENT_VERBINDER = StückGut("ElementVerbinder")
 
 
 def teileliste(*, korpus, fach_kanten, deck, front, füll):
-
 
     hinterwand = korpus.brett(ELEMENT)
     deckteil = deck.brett(ELEMENT)
